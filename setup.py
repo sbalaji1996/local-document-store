ebd4d0632f55b3b2ed88209de63fb89cc0f349a3
Srinand Balaji <srinand.balaji@gmail.com>
2018-03-19 00:20:49 -0700

    refactored module placement, added python requirement.

--- setup.py
+++ setup.py
@@ -1,8 +1,8 @@
 from setuptools import setup
 
 setup(
-  name='local-document-store',
-  packages=[],
+  name='lds',
+  packages=['lds'],
   version='0.0.1',
   description='A package written in Python that mimics some of the functionality provided by MongoDB.',
   author='Srinand Balaji',
@@ -11,4 +11,5 @@ setup(
   download_url='https://github.com/sbalaji1996/local-document-store/archive/0.0.1.tar.gz',
   keywords=['json', 'document', 'in-memory', 'mongodb'],
   classifiers=[],
+  python_requires='>=3.5'
 )
