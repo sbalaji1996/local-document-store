85c37e4c549f9b098b922ceb42af3f9da4fc3839
Srinand Balaji <srinand.balaji@gmail.com>
2018-03-18 23:14:38 -0700

    updated README, added setup files.

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+  name='local-document-store',
+  packages=[],
+  version='0.0.1',
+  description='A package written in Python that mimics some of the functionality provided by MongoDB.',
+  author='Srinand Balaji',
+  author_email='srinand@berkeley.edu',
+  url='https://github.com/sbalaji1996/local-document-store',
+  download_url='https://github.com/sbalaji1996/local-document-store/archive/0.0.1.tar.gz',
+  keywords=['json', 'document', 'in-memory', 'mongodb'],
+  classifiers=[],
+)
